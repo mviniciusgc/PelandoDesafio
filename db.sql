8f43e53dd1abfe5e8d45f93b795c32ef571e244b
Marcos Cunha <mviniciusgc@hotmail.com>
2023-03-20 15:33:29 -0300

    refactor: alterando campos no banco de dados

--- db.sql
+++ db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE promotion (
-	id SERIAL CONSTRAINT pk_id_promotion PRIMARY KEY,
+	promotion_id SERIAL CONSTRAINT pk_id_promotion PRIMARY KEY,
 	url TEXT NULL,
+	img TEXT NULL,
 	title TEXT NULL,
 	description TEXT NULL,
 	price  VARCHAR(250) NULL,
